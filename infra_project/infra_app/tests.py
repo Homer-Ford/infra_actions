1496712d0fe88c7b560a96673047ef8ebf79919d
Anton Anuchin <anton-anuchin@mail.ru>
2022-07-28 09:24:44 +0300

    add deploy

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -13,10 +13,3 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, 404)
 
-    def test_page_shows_correct_content(self):
-        """Проверка контента страниц."""
-        response = self.guest_client.get('/')
-        self.assertContains(response, 'У меня получилось!')
-
-        response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 'А это вторая страница!')
